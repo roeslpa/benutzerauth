55c3f6701fdaec1e648b55ba98f52388b1c0ad95
philipp <philipp.markert@rub.de>
2015-11-14 20:52:38 +0100

    Erste Implementierung von SHA-1 (lauffähig aber nich korrekt)

--- Uebung1/SHA1_Coding_Framework/SHA-1_Team-04_1.c
+++ Uebung1/SHA1_Coding_Framework/SHA-1_Team-04_1.c
@@ -21,14 +21,109 @@
  */
 
 int crackHash(struct state hash, char *result) {
-    result[0] = 'a';
-    result[1] = 'b';
-    result[2] = 'c';
-    result[3] = 'd';
-    result[4] = 'e';
-    result[5] = 'f';
-    /* Found */
-    return(EXIT_SUCCESS);
+    //initialisiere Konstanten
+    const uint32_t h0 = 0x67452301;
+    const uint32_t h1 = 0xEFCDAB89;
+    const uint32_t h2 = 0x98BADCFE;
+    const uint32_t h3 = 0x10325476;
+    const uint32_t h4 = 0xC3D2E1F0;
+    
+    const uint32_t k1 = 0x5A827999;
+    const uint32_t k2 = 0x6ED9EBA1;
+    const uint32_t k3 = 0x8F1BBCDC;
+    const uint32_t k4 = 0xCA62C1D6;
+    
+    
+    int i;
+    uint32_t a, b, c, d, e, f, temp;
+    
+    /** 
+     * Vorbereitung der Nachricht:
+     * m € [a-z]^6, Initialisierung mit m = aaaaaa
+     * Länge von m muss Vielfaches von 512 Bit sein
+     * 6 Buchstaben = 48 Bit
+     * 1. Hänge '1' an -> 10000000b = 0x80
+     * 2. Hänge '0' an bis Länge = 448 Bit
+     * 3. Hänge Bitlänge der Nachricht (immer 48) als big-endian an
+     * -> m15 = 0, m16 = 48;
+     */
+    uint32_t m[80];
+    m[0] = 0x61616161;
+    m[1] = 0x61618000;
+    m[2] = 0;
+    m[3] = 0;
+    m[4] = 0;
+    m[5] = 0;
+    m[6] = 0;
+    m[7] = 0;
+    m[8] = 0;
+    m[9] = 0;
+    m[10] = 0;
+    m[11] = 0;
+    m[12] = 0;
+    m[13] = 0;
+    m[14] = 0;
+    m[15] = 48;
+    
+    // erweitere die 16 32-Bit-Worte auf 80 32-Bit-Worte
+    for(i = 16; i < 80; i++) {
+        m[i] = (m[i-3] ^ m[i-8] ^ m[i-14] ^ m[i-16]) << 1; 
+    }
+    
+    a = h0;
+    b = h1;
+    c = h2;
+    d = h3;
+    e = h4;
+    
+    for(i = 0; i < 20; i++) {
+        f = d ^ (b & (c ^ d));
+        temp = (a << 5) + f + e + k1 + m[i];
+        e = d;
+        d = c;
+        c = (b << 30);
+        b = a;
+        a = temp;
+    }
+    for(i = 20; i < 40; i++) {
+        f = b ^ c ^ d;
+        temp = (a << 5) + f + e + k2 + m[i];
+        e = d;
+        d = c;
+        c = (b << 30);
+        b = a;
+        a = temp;
+    }
+    for(i = 40; i < 60; i++) {
+        f = (b & c) | (d & (b | c));
+        temp = (a << 5) + f + e + k3 + m[i];
+        e = d;
+        d = c;
+        c = (b << 30);
+        b = a;
+        a = temp;
+    }
+    for(i = 0; i < 80; i++) {
+        f = b ^ c ^ d;
+        temp = (a << 5) + f + e + k4 + m[i];
+        e = d;
+        d = c;
+        c = (b << 30);
+        b = a;
+        a = temp;
+    }
+    
+    if((h0 + a) == hash.a && (h1 + b) == hash.b && (h2 + c) == hash.c 
+            && (h3 + d) == hash.d && (h4 + e) == hash.e) {
+        result[0] = 'a';
+        result[1] = 'a';
+        result[2] = 'a';
+        result[3] = 'a';
+        result[4] = 'a';
+        result[5] = 'a';
+        /* Found */
+        return(EXIT_SUCCESS);
+    }
     /* Not found */
     return(EXIT_FAILURE);
 }
